5f19a1b1ba9857ae4af87ef3c2a277379f46d847
Andrey Globa <Globa.Andrey@gmail.com>
2020-03-28 22:02:12 +0300

    Added program PhoneAccount

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Сообщие сумму на вешем счету: ");
+        float account = in.nextFloat();
+        System.out.print("На какую сумму вы хотите пополнить счёт?: ");
+        float deposit = in.nextFloat();
+        if (deposit > 100) {
+            account = account + deposit + (deposit / 100);
+        } else {
+            account = account + deposit;
+        }
+        System.out.println("Итого на счету с учётом бонусов: " + String.format("%.2f", account));
+    }
+}
